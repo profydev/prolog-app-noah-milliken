ed7f286889345ccebaed821aaa75cdb677cba433
noah milliken <noahmilliken@gmail.com>
2023-11-16 21:45:49 -0500

    fix-sidebar-arrow Add conditional props to the MenuItem component comtaining the sidebar open/close toggle button The arrow SVG points right when menu is closed.

--- features/layout/sidebar-navigation/sidebar-navigation.tsx
+++ features/layout/sidebar-navigation/sidebar-navigation.tsx
@@ -87,10 +87,13 @@ export function SidebarNavigation() {
             />
             <MenuItemButton
               text="Collapse"
-              iconSrc="/icons/arrow-left.svg"
+              iconSrc={"/icons/arrow-left.svg"}
               isCollapsed={isSidebarCollapsed}
               onClick={() => toggleSidebar()}
-              className={styles.collapseMenuItem}
+              className={classNames(
+                styles.collapseMenuItem,
+                isSidebarCollapsed && styles.iconRotate,
+              )}
             />
           </ul>
         </nav>
